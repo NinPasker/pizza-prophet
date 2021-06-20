1d6621372b8cfcc005af14fb2e9fbbf3682cc4ce
Nin Pasker <gooddev.e.nin@gmail.com>
2021-06-20 15:48:46 -0400

    Added result to page.

--- script.js
+++ script.js
@@ -38,28 +38,40 @@ const randomElement = (array) => {
 	return array[randomIndex];
 }
 
-//  Listen for form submit 
-const form = document.querySelector('form');
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	// Once form submits grab values from selected radio inputs.
-	const sizeSelected = document.querySelector('input[name=size]:checked').value;
-	const priceSelected = document.querySelector('input[name=price]:checked').value;
+const pizzaProphetInit = () => {
+	//  Listen for form submit 
+	const form = document.querySelector('form');
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		// Once form submits grab values from selected radio inputs.
+		const sizeSelected = document.querySelector('input[name=size]:checked').value;
+		const priceSelected = document.querySelector('input[name=price]:checked').value;
 
-	console.log(sizeSelected, priceSelected);
-	// Use radio inpit values, to look through pizza object 
+		console.log(sizeSelected, priceSelected);
+		// Use radio inpit values, to look through pizza object 
 
-	const sizePizzaList = pizza[sizeSelected];
+		const sizePizzaList = pizza[sizeSelected];
 
-	console.log(sizePizzaList);
+		console.log(sizePizzaList);
 
-	const pricePizzaList = sizePizzaList.filter((pizzaPlace) => {
-		pizzaPlace.price === priceSelected
-	});
+		const pricePizzaList = sizePizzaList.filter((pizzaPlace) =>
+			pizzaPlace.price === priceSelected);
 
-	const finalPizzaPlace = randomElement(pricePizzaList);
-	console.log("FINAL: ", finalPizzaPlace);
+		const finalPizzaPlace = randomElement(pricePizzaList);
+		console.log("FINAL: ", finalPizzaPlace);
 
-	
-})
-// Display the matching pizza joint to user
+		// Display the matching pizza joint to user
+
+		const pizzaHtml = `<h2>Go Here! ➡ <span class="restaurant">${finalPizzaPlace.title}</span></h2>`;
+		
+		document.querySelector('.results').innerHTML = pizzaHtml;
+	})
+}
+
+if (document.readyState === 'complete') {
+	// IE 8 Support
+	pizzaProphetInit();
+} else {
+	// Other modern browsers
+	document.addEventListener('DOMContentLoaded', pizzaProphetInit);
+}
